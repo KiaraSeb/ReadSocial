f03dc0fdb93a8151d8772189756b1a032172798e
Amir <cm.nt.amir@gmail.com>
2024-11-20 09:49:49 -0300

    cambios_Front-End

--- ReadSocial/frontend/app.js
+++ ReadSocial/frontend/app.js
@@ -2,34 +2,34 @@
 import { login, getProtectedData } from './api.js';
 
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+  e.preventDefault();
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
 
-    try {
-        const response = await login(username, password);
-        if (response.token) {
-            localStorage.setItem('token', response.token); // Guarda el token
-            alert('Inicio de sesión exitoso');
-        } else {
-            alert('Credenciales incorrectas');
-        }
-    } catch (error) {
-        console.error('Error en el inicio de sesión:', error);
+  try {
+    const response = await login(username, password);
+    if (response.token) {
+      localStorage.setItem('token', response.token); // Guarda el token
+      alert('Inicio de sesión exitoso');
+    } else {
+      alert('Credenciales incorrectas');
     }
+  } catch (error) {
+    console.error('Error en el inicio de sesión:', error);
+  }
 });
 
 document.getElementById('fetchDataButton').addEventListener('click', async () => {
-    const token = localStorage.getItem('token');
-    if (!token) {
-        alert('Por favor, inicia sesión primero');
-        return;
-    }
+  const token = localStorage.getItem('token');
+  if (!token) {
+    alert('Por favor, inicia sesión primero');
+    return;
+  }
 
-    try {
-        const data = await getProtectedData(token);
-        console.log('Datos protegidos:', data);
-    } catch (error) {
-        console.error('Error al obtener datos protegidos:', error);
-    }
+  try {
+    const data = await getProtectedData(token);
+    console.log('Datos protegidos:', data);
+  } catch (error) {
+    console.error('Error al obtener datos protegidos:', error);
+  }
 });
